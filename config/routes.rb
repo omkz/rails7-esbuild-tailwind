d061a794f2af6345b86c24814f60a881960e0785
Kurnia Muhamad <omenkzz@gmail.com>
2022-01-18 14:40:44 +0700

    Upgrade ruby n rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  root "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Almost every application defines a route for the root path ("/") at the top of this file.
+  # Defines the root path route ("/")
   # root "articles#index"
 end
