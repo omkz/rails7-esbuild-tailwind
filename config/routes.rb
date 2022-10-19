f20a2ce65c0352faa8cdac2b236ef28ac6da96c2
Kurnia Muhamad <kurnia.prague@gmail.com>
2022-10-19 14:54:57 +0700

    Add store page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/about'
+  get 'pages/store'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
